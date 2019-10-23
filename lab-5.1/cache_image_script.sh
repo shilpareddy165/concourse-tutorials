6568bd2e525418fe19c3c553be3e0068e295e2fc
Khozema Nullwala <khozema.nullwala@accenture.com>
2019-10-23 12:45:12 +0530

    Added Files for Lab Activity 5.1

---
+++ lab-5.1/cache_image_script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+output_dir=workspace
+mvn -Dmaven.repo.local=$(pwd)/simple-java-app/cache -DskipTests -f $(pwd)/simple-java-app/pom.xml package
+mv $(pwd)/simple-java-app/cache "${output_dir}/"
+cp $(pwd)/concourse-tutorials/lab-5.1/docker/CacheDockerfile "${output_dir}/Dockerfile"
